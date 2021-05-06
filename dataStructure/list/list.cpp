d5af711e21f8f241a0f71b907d1f357047369b4b
bypb0307 <bypb_chen@163.com>
2021-05-06 15:57:10 +0800

    modify list func

--- dataStructure/list/list.cpp
+++ dataStructure/list/list.cpp
@@ -4,32 +4,65 @@
 using namespace std;
 
 //初始化链表
-void List::initList(ListNode *head, int size)
+ListNode *List::initList(int len)
 {
-	static int s = 0;
+	if (len < 1) //如果初始化链表长度小于1，则为空链表
+	{
+		return nullptr;
+	}
+
+	static int s = 0; //防止在同一秒调用多次生成一样的随机链表
 	srand(time(0) + s);
 	s++;
+
 	int tmp;
-	tmp = rand() % 100 + 1;
-	head->val = tmp;
+	tmp = rand() % 100 + 1;			 //生成一个1-99的随机数作为结点数据
+	ListNode *head = new ListNode(); //初始化头结点
+	head->data = tmp;
 	head->next = nullptr;
 	ListNode *p = head;
 
-	for (int i = 0; i < size - 1; i++)
+	for (int i = 0; i < len - 1; i++) //循环初始化剩余结点
 	{
 		tmp = rand() % 100 + 1;
-		ListNode *q = new ListNode(tmp);
-		p->next = q;
-		p = q;
+		ListNode *node = new ListNode();
+		node->data = tmp;
+		node->next = nullptr;
+		p->next = node;
+		p = node;
 	}
+	return head;
 }
-//反转链表，直接在原有链表上反转
+
+//反转链表，不改变原有链表，建立新空间。也可以利用stack
 ListNode *List::reverseList1(ListNode *head)
 {
+	if (head == nullptr || head->next == nullptr)
+	{
+		return nullptr;
+	}
+	ListNode *ret = nullptr;
+	while (head != nullptr)
+	{
+		ListNode *node = new ListNode();
+		node->data = head->data;
+		node->next = ret;
+		ret = node;
+		head = head->next;
+	}
+	return ret;
+}
+
+//反转链表，直接在原有链表上反转
+ListNode *List::reverseList2(ListNode *head)
+{
+	if (head == nullptr || head->next == nullptr)
+	{
+		return nullptr;
+	}
 	ListNode *pre = nullptr;
 	ListNode *cur = head;
-	ListNode *tmp;
-
+	ListNode *tmp = nullptr;
 	while (cur != nullptr)
 	{
 		tmp = cur->next;
@@ -39,72 +72,128 @@ ListNode *List::reverseList1(ListNode *head)
 	}
 	return pre;
 }
-//反转链表，不改变原有链表，建立新空间，利用stack
-ListNode *reverseList2(const ListNode *head)
-{
-	if (head == nullptr || head->next == nullptr)
-	{
-		return head;
-	}
-	stack<int> datas;
-	ListNode *p = head;
-	while (p != nullptr)
-	{
-		datas.push(p->val);
-		p = p->next;
-	}
 
-	int tmp = datas.pop();
-	ListNode *q = new ListNode(tmp);
-	ListNode *t = q;
-	while(!datas.empty())
-	{
-		
-	}
-}
-
-//合并两个有序链表
-ListNode *List::mergeTwoList(ListNode *head1, ListNode *head2)
+//合并两个有序链表。循环方式。会破坏原来的两个list
+ListNode *List::mergeTwoList1(ListNode *head1, ListNode *head2)
 {
 	if (head1 == nullptr)
 		return head2;
 	if (head2 == nullptr)
 		return head1;
-	return nullptr;
 
-	/*
-	// 这种会破坏原来的两个list
-	ListNode* head;
-	if(head1->val > head2->val)
+	ListNode *newHead = new ListNode(); //创建一个头结点，返回的时候返回头结点的下一个结点，并且释放该头结点
+	ListNode *p = newHead;
+
+	while (head1 && head2)
 	{
-		head = head2;
-		head->next = mergeTwoList(head1, head2->next);
+		if (head1->data > head2->data)
+		{
+			p->next = head2;
+			head2 = head2->next;
+		}
+		else
+		{
+			p->next = head1;
+			head1 = head1->next;
+		}
+		p = p->next;
+	}
+	if (head1 == nullptr)
+	{
+		p->next = head2;
 	}
 	else
 	{
-		head = head1;
-		head->next = mergeTwoList(head1->next, head2);
+		p->next = head1;
 	}
-	return head;
-	*/
-
-	// ListNode* head;
-	// if(head1->val > head2->val)
-	// {
-	// head = new ListNode(head2->val);
-	// ListNode* tmp = mergeTwoList(head1, head2->next);
-	// head->next = new ListNode(tmp->val);
-	// }
-	// else
-	// {
-	// head = new ListNode(head1->val);
-	// ListNode* tmp = mergeTwoList(head2, head1->next);
-	// head->next = new ListNode(tmp->val);
-	// }
-	// return head;
+	ListNode *h = newHead;
+	newHead = newHead->next;
+	delete h;
+	return newHead;
 }
 
-//合并多个有序链表,分治
+//合并两个有序链表。递归方式。会破坏原来的两个list
+ListNode *List::mergeTwoList2(ListNode *head1, ListNode *head2)
+{
+	if (head1 == nullptr)
+		return head2;
+	if (head2 == nullptr)
+		return head1;
+
+	if (head1->data > head2->data)
+	{
+		head2->next = mergeTwoList2(head1, head2->next);
+		return head2;
+	}
+	else
+	{
+		head1->next = mergeTwoList2(head1->next, head2);
+		return head1;
+	}
+}
+
+//合并两个有序链表。循环方式。不会破坏原来的两个list
+ListNode *List::mergeTwoList3(ListNode *head1, ListNode *head2)
+{
+	if (head1 == nullptr)
+	{
+		return head2;
+	}
+	else if (head2 == nullptr)
+	{
+		return head1;
+	}
+	ListNode *newHead = new ListNode();
+	ListNode *p = newHead;
+	ListNode *tmp;
+
+	while (head1 && head2)
+	{
+		if (head1->data > head2->data)
+		{
+			tmp = new ListNode();
+			tmp->data = head2->data;
+			p->next = tmp;
+			head2 = head2->next;
+		}
+		else
+		{
+			tmp = new ListNode();
+			tmp->data = head1->data;
+			p->next = tmp;
+			head1 = head1->next;
+		}
+		p = p->next;
+	}
+	while (head1)
+	{
+		tmp = new ListNode();
+		tmp->data = head1->data;
+		p->next = tmp;
+		p = p->next;
+		head1 = head1->next;
+	}
+	while (head2)
+	{
+		tmp = new ListNode();
+		tmp->data = head2->data;
+		p->next = tmp;
+		p = p->next;
+		head2 = head2->next;
+	}
+	ListNode *h = newHead;
+	newHead = newHead->next;
+	delete h;
+	return newHead;
+}
+
+//合并两个有序链表。循环方式。不会破坏原来的两个list
+ListNode *List::mergeTwoList4(ListNode *head1, ListNode *head2)
+{
+	return nullptr;
+}
+
+//合并多个有序链表
 ListNode *List::merge(vector<ListNode *> vec)
 {
 	return nullptr;
@@ -113,23 +202,23 @@ ListNode *List::merge(vector<ListNode *> vec)
 //链表排序
 void List::bubbleSort(ListNode **head)
 {
-	ListNode *p;
-	ListNode *q;
-	ListNode *r;
 	if ((*head) == nullptr || (*head)->next == nullptr)
 	{
 		return;
 	}
-	bool sorted = true;
+
+	ListNode *p;
+	ListNode *q;
+	bool sorted = false;
 	for (p = *head; p != nullptr; p = p->next)
 	{
-		for (q = *head; q->next != nullptr; q = q->next)
+		for (q = p->next; q != nullptr; q = q->next)
 		{
-			if (q->val > q->next->val)
+			if (p->data > q->data)
 			{
-				int tmp = q->val;
-				q->val = q->next->val;
-				q->next->val = tmp;
+				int tmp = p->data;
+				p->data = q->data;
+				q->data = tmp;
 				sorted = false;
 			}
 		}
@@ -138,20 +227,18 @@ void List::bubbleSort(ListNode **head)
 			break;
 		}
 	}
-	cout << "after bubbleSort" << endl;
-	printList(*head);
 }
+
 //打印链表
 void List::printList(ListNode *head)
 {
-	cout << "print list: ";
 	if (head == nullptr)
 	{
 		return;
 	}
 	while (head != nullptr)
 	{
-		cout << head->val << " ";
+		cout << head->data << " ";
 		head = head->next;
 	}
 	cout << endl;
@@ -160,30 +247,45 @@ void List::printList(ListNode *head)
 int main()
 {
 	List li;
-	ListNode *head1 = new ListNode();
-	li.initList(head1, 10);
+	ListNode *head1 = li.initList(10); //初始化一个列表head1
+	cout << "print head1:" << endl;
+	li.printList(head1);
+	li.bubbleSort(&head1); //对列表head1进行冒泡排序
+	cout << "print bubble sorted head1:" << endl;
 	li.printList(head1);
 
-	li.bubbleSort(&head1);
+	ListNode *head1Rev1 = li.reverseList1(head1); //逆转链表head1，不改变原有链表
+	cout << "print head1 reverseList1:" << endl;
+	li.printList(head1);
+	cout << "print head1Rev1 after reverseList1:" << endl;
+	li.printList(head1Rev1);
 
-	head1 = li.reverseList1(head1);
+	ListNode *head1Rev2 = li.reverseList2(head1); //直接在原有链表基础上逆转链表head1，会改变head1的原有结构
+	cout << "print head1 reverseList2:" << endl;
+	li.printList(head1);
+	cout << "print head1Rev1 after reverseList2:" << endl;
+	li.printList(head1Rev2);
 
-	// ListNode *head2 = new ListNode();
-	// li.initList(head2, 10);
-	// li.printList(head2);
-	// li.bubbleSort(&head2);
+	cout << "merge two list............................." << endl
+		 << endl;
 
-	// cout << endl;
+	ListNode *head2 = li.initList(10); //初始化一个列表head2
+	li.bubbleSort(&head2);			   //对列表head2进行冒泡排序
+	cout << "print sorted list head2:" << endl;
+	li.printList(head2);
 
-	// ListNode *head1_r = li.reverseList(head1);
-	// li.printList(head1_r);
-	// li.printList(head1);
+	ListNode *head3 = li.initList(10); //初始化一个列表head3
+	li.bubbleSort(&head3);			   //对列表head3进行冒泡排序
+	cout << "print sorted list head3:" << endl;
+	li.printList(head3);
 
-	// ListNode* head = li.mergeTwoList(head1, head2);
-	// li.printList(head);
-	// cout<<"mergeTwoList end"<<endl;
-	// li.printList(head1);
-	// li.printList(head2);
+	ListNode *head23 = li.mergeTwoList4(head2, head3); //测试合并两个有序链表 mergeTwoList1 mergeTwoList2 mergeTwoList3
+	cout << "print merged list head23:" << endl;
+	li.printList(head23);
+	cout << "print merged head2:" << endl;
+	li.printList(head2);
+	cout << "print merged head3:" << endl;
+	li.printList(head3);
 
 	return 0;
 }
--- dataStructure/list/list.h
+++ dataStructure/list/list.h
@@ -1,31 +1,40 @@
 /*
  * @Author: chenq
  * @Date: 2020-12-03 23:33:00
- * @LastEditTime: 2020-12-05 22:52:46
- * @Descripttion: 
+ * @LastEditTime: 2021-05-06 15:38:53
+ * @Descripttion: 链表反转，合并有序链表
  */
 #include <iostream>
 #include <vector>
 #include <stack>
 using namespace std;
 
-
+//链表结点
 struct ListNode
-{	
-    int val;
+{
+	ListNode()
+	{
+		data = 0;
+		next = nullptr;
+	};
+	int data;
     ListNode* next;
-	ListNode(){};
-    ListNode(int x):val(x),next(nullptr){}
 };
 
 class List
 {
 public:
-	void initList(ListNode* head, int size);	//初始化链表
-	ListNode* reverseList1(ListNode* head);		//反转链表，直接在原有链表上反转
-	ListNode* reverseList2(ListNode* head);		//反转链表，不改变原有链表，建立新空间，利用stack
-	ListNode* mergeTwoList(ListNode* head1, ListNode* head2);	//合并两个有序链表
-	ListNode* merge(vector<ListNode*> vec);	//合并多个有序链表,分治
+	ListNode* initList(int len);	//初始化链表
+	
+	ListNode* reverseList1(ListNode* head);		//反转链表，不改变原有链表，建立新空间。也可以利用stack
+	ListNode* reverseList2(ListNode* head);		//反转链表，直接在原有链表上反转
+
+	ListNode* mergeTwoList1(ListNode* head1, ListNode* head2);	//合并两个有序链表。循环方式。会破坏原来的两个list
+	ListNode* mergeTwoList2(ListNode* head1, ListNode* head2);	//合并两个有序链表。递归方式。会破坏原来的两个list
+	ListNode* mergeTwoList3(ListNode* head1, ListNode* head2);	//合并两个有序链表。循环方式。不会破坏原来的两个list
+	ListNode* mergeTwoList4(ListNode* head1, ListNode* head2);	//合并两个有序链表。递归方式。不会破坏原来的两个list
+
+	ListNode* merge(vector<ListNode*> vec);	//合并多个有序链表
 	
 	
 	void bubbleSort(ListNode** head);	//链表排序
