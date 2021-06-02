95fd1c3ac455ccb93dba3d59c685f49b8476af19
bypb0307 <bypb_chen@163.com>
2021-06-02 23:16:42 +0800

    modify sort

--- dataStructure/sorting/sort.cpp
+++ dataStructure/sorting/sort.cpp
@@ -1,242 +1,239 @@
 #include "sort.h"
 
 //冒泡排序
-void Sort::bubbleSort(int* a, int size)
+void Sort::bubbleSort(int *a, int len)
 {
 	bool sorted = true;
-	for(int i=size-1;i>0;i--)
+	for (int i = len - 1; i > 0; i--)
 	{
-		for(int j=0;j<i;j++)
+		for (int j = 0; j < i; j++)
 		{
-			if(a[j] > a[j+1])
+			if (a[j] > a[j + 1])
 			{
-				swapVal(a[j], a[j+1]);
+				swapVal(a[j], a[j + 1]);
 				sorted = false;
 			}
 		}
-		if(sorted)
+		if (sorted)
 		{
 			break;
 		}
 	}
-	
-	printArray(a, size);
-	cout<<"bubbleSort end ..."<<endl;
+
+	printArray(a, len);
+	cout << "bubbleSort end ..." << endl;
 }
 //选择排序
-void Sort::selectSort(int* a, int size)
+void Sort::selectSort(int *a, int len)
 {
 	int max_idx;
-	for(int i=size-1;i>0;i--)
+	for (int i = len - 1; i > 0; i--)
 	{
-		max_idx = i;
-		for(int j=0;j<i;j++)
+		max_idx = 0;
+		for (int j = 1; j <= i; j++)
 		{
-			if(a[j] > a[max_idx])
+			if (a[j] > a[max_idx])
 			{
 				max_idx = j;
 			}
 		}
-		if(max_idx != i)
-		{
-			swapVal(a[max_idx], a[i]);
-		}
+		swapVal(a[max_idx], a[i]);
 	}
-	
-	printArray(a, size);
-	cout<<"selectSort end ..."<<endl;
+
+	printArray(a, len);
+	cout << "selectSort end ..." << endl;
 }
 //插入排序
-void Sort::insertSort(int* a, int size)
+void Sort::insertSort(int *a, int len)
 {
 	int i, j, tmp;
-	for(i=1;i<size;i++)
+	for (i = 1; i < len; i++)
 	{
 		tmp = a[i];
-		for(j=i;j>0&&a[j-1]>tmp;j--)
+		for (j = i; j > 0 && a[j - 1] > tmp; j--)
 		{
-			a[j]=a[j-1];
+			a[j] = a[j - 1];
 		}
-		a[j]=tmp;
+		a[j] = tmp;
 	}
-	
-	printArray(a, size);
-	cout<<"insertSort end ..."<<endl;
+
+	printArray(a, len);
+	cout << "insertSort end ..." << endl;
 }
 //快速排序
-void Sort::quickSort(int* a, int start, int end)
+void Sort::quickSort(int *a, int start, int end)
 {
-	if(start >= end)
+	if (start >= end)
 	{
 		return;
 	}
 	int pivot = a[start];
 	int low = start;
 	int high = end;
-	while(low < high)
+	while (low < high)
 	{
-		while((low<high) && (a[high]>=pivot))
+		while ((low < high) && (a[high] >= pivot))
 		{
 			high--;
 		}
-		if(low < high)
+		if (low < high)
 		{
 			a[low] = a[high];
 		}
-		while((low<high) && (a[low]<pivot))
+		while ((low < high) && (a[low] < pivot))
 		{
 			low++;
 		}
-		if(low < high)
+		if (low < high)
 		{
 			a[high] = a[low];
 		}
-		if(low == high)
+		if (low == high)
 		{
 			a[low] = pivot;
 		}
 	}
-	quickSort(a, start, low-1);
-	quickSort(a, low+1, end);
-	
+	quickSort(a, start, low - 1);
+	quickSort(a, low + 1, end);
+
 	printArray(a, start, end);
-	cout<<"quickSort end ..."<<endl;
+	cout << "quickSort end ..." << endl;
 }
 //希尔排序
-void Sort::hellSort(int* a, int size)
+void Sort::hellSort(int *a, int len)
 {
-	if(size < 2)
+	if (len < 2)
 	{
 		return;
 	}
-	int i,j,tmp;
-	int gap = size/2;
-	while(gap > 0)
+	int i, j, tmp;
+	int gap = len / 2;
+	while (gap > 0)
 	{
-		for(i=gap;i<=size-gap;i+=gap)
+		for (i = gap; i <= len - gap; i += gap)
 		{
 			tmp = a[i];
-			for(j=i;j>=gap&&a[j-gap]>tmp;j-=gap)
+			for (j = i; j >= gap && a[j - gap] > tmp; j -= gap)
 			{
-				a[j]=a[j-gap];
+				a[j] = a[j - gap];
 			}
-			a[j]=tmp;
+			a[j] = tmp;
 		}
-		gap = gap/2;
+		gap = gap / 2;
 	}
-	
-	printArray(a, size);
-	cout<<"hellSort end ..."<<endl;
+
+	printArray(a, len);
+	cout << "hellSort end ..." << endl;
 }
 //归并排序
-void Sort::mergeSort(int* a, int start, int end)
+void Sort::mergeSort(int *a, int start, int end)
 {
-	if(start >= end)
+	if (start >= end)
 	{
 		return;
 	}
-	int mid = (start+end)/2;
+	int mid = (start + end) / 2;
 	mergeSort(a, start, mid);
-	mergeSort(a, mid+1, end);
+	mergeSort(a, mid + 1, end);
 	merge(a, start, mid, end);
 }
-void Sort::merge(int* a, int start, int mid, int end)
+void Sort::merge(int *a, int start, int mid, int end)
 {
 	int n = end - start + 1;
-	int* tmp = new int[n];
+	int *tmp = new int[n];
 	int left = start;
-	int right = mid+1;
-	int i=0;
-	while(left<=mid && right<=end)
+	int right = mid + 1;
+	int i = 0;
+	while (left <= mid && right <= end)
 	{
 		tmp[i++] = a[left] < a[right] ? a[left++] : a[right++];
 	}
-	while(left <= mid)
+	while (left <= mid)
 	{
 		tmp[i++] = a[left++];
 	}
-	while(right <= end)
+	while (right <= end)
 	{
 		tmp[i++] = a[right++];
 	}
-	
-	for(int j=0;j<n;j++)
+
+	for (int j = 0; j < n; j++)
 	{
-		a[start+j]=tmp[j];
+		a[start + j] = tmp[j];
 	}
-	delete [] tmp;
+	delete[] tmp;
 }
 //堆排序，大顶堆
-void Sort::maxHeapSort(int* a, int size)
+void Sort::maxHeapSort(int *a, int len)
 {
 	//构建大顶堆，从最后一个父节点开始
-	for(int i=size/2-1;i>=0;i--)
+	for (int i = len / 2 - 1; i >= 0; i--)
 	{
-		maxHeapify(a, i, size);
+		maxHeapify(a, i, len);
 	}
-	
+
 	//取大顶堆的根节点，即最大的数，放在数组最后的位置
-	for(int j=size-1;j>0;j--)
+	for (int j = len - 1; j > 0; j--)
 	{
 		swapVal(a[j], a[0]);
-		maxHeapify(a, 0, j);	//去除最后位置的元素，重新建堆，只需要从根节点开始，后面的都是已经有序的
+		maxHeapify(a, 0, j); //去除最后位置的元素，重新建堆，只需要从根节点开始，后面的都是已经有序的
 	}
-	
-	printArray(a, size);
-	cout<<"maxHeapSort end ..."<<endl;
+
+	printArray(a, len);
+	cout << "maxHeapSort end ..." << endl;
 }
-void Sort::maxHeapify(int* a, int root, int size)
+void Sort::maxHeapify(int *a, int root, int len)
 {
-	int lchild = 2*root+1;
-	if(lchild >= size)
+	int lchild = 2 * root + 1;
+	if (lchild >= len)
 	{
 		return;
 	}
 	int max_child = lchild;
-	int rchild = lchild+1;
-	if(rchild < size && a[rchild]>a[max_child])
+	int rchild = lchild + 1;
+	if (rchild < len && a[rchild] > a[max_child])
 	{
 		max_child = rchild;
 	}
-	if(a[root]<a[max_child])
+	if (a[root] < a[max_child])
 	{
 		swapVal(a[root], a[max_child]);
-		maxHeapify(a, max_child, size);
+		maxHeapify(a, max_child, len);
 	}
 }
 
 //交换两个整数的值
-void Sort::swapVal(int& a, int& b)
+void Sort::swapVal(int &a, int &b)
 {
 	int tmp = a;
 	a = b;
 	b = tmp;
 }
 
-void Sort::printArray(int* a, int size)
+void Sort::printArray(int *a, int len)
 {
-	for(auto i = 0; i < size; i++)
+	for (auto i = 0; i < len; i++)
 	{
-		cout<<a[i]<<" ";
+		cout << a[i] << " ";
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-void Sort::printArray(int* a, int start, int end)
+void Sort::printArray(int *a, int start, int end)
 {
-	for(auto i = start; i <= end; i++)
+	for (auto i = start; i <= end; i++)
 	{
-		cout<<a[i]<<" ";
+		cout << a[i] << " ";
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-void initArray(int* a, int size)
+void initArray(int *a, int len)
 {
 	srand(time(0));
-	for(int i=0;i<size;i++)
+	for (int i = 0; i < len; i++)
 	{
-		a[i] = rand()%100 + 1; 
+		a[i] = rand() % 100 + 1;
 	}
 }
 
@@ -245,22 +242,21 @@ int main()
 	int a[10];
 	initArray(a, 10);
 	Sort st;
-	cout<<"print original array: ";
+	cout << "print original array: ";
 	st.printArray(a, 10);
 
-	cout<<" print after sorting: ";	
+	cout << " print after sorting: ";
 	// st.bubbleSort(a, 10);
-	// st.selectSort(a, 10);
+	st.selectSort(a, 10);
 	// st.insertSort(a, 10);
 	// st.quickSort(a, 0, 9);
 	// st.hellSort(a, 10);
-	
+
 	// st.mergeSort(a, 0, 9);
 	// st.printArray(a, 0, 9);
 	// cout<<"mergeSort end ..."<<endl;
-	
-	st.maxHeapSort(a, 10);
-	
-	
+
+	// st.maxHeapSort(a, 10);
+
 	return 0;
 }
--- dataStructure/sorting/sort.h
+++ dataStructure/sorting/sort.h
@@ -1,3 +1,9 @@
+/*
+ * @Author: chenq
+ * @Date: 2021-05-10 21:39:54
+ * @LastEditTime: 2021-05-10 21:42:32
+ * @Descripttion: 
+ */
 #include <iostream>
 using namespace std;
 
@@ -7,17 +13,17 @@ public:
 	Sort(){}
 	~Sort(){}
 	
-	void bubbleSort(int* a, int size);	//冒泡排序
-	void selectSort(int* a, int size);	//选择排序
-	void insertSort(int* a, int size);	//插入排序
+	void bubbleSort(int* a, int len);	//冒泡排序
+	void selectSort(int* a, int len);	//选择排序
+	void insertSort(int* a, int len);	//插入排序
 	void quickSort(int* a, int start, int end);	//快速排序
-	void hellSort(int* a, int size);		//希尔排序
+	void hellSort(int* a, int len);		//希尔排序
 	void mergeSort(int* a, int start, int end);		//归并排序
 	void merge(int* a, int start, int mid, int end);
-	void maxHeapSort(int* a, int size);	//堆排序
-	void maxHeapify(int* a, int root, int size);	//构建大根堆
+	void maxHeapSort(int* a, int len);	//堆排序
+	void maxHeapify(int* a, int root, int len);	//构建大根堆
 	
-	void printArray(int* a, int size);	//打印数组
+	void printArray(int* a, int len);	//打印数组
 	void printArray(int* a, int start, int end);	//打印数组
 	void swapVal(int& a, int& b);		//交换两个整数的值
 };
